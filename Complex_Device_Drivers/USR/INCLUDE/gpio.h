530eae5a58503ae3b2bff5decb07a42cf7b9fbf2
Suhas Bharadwaj K <tak.kumara@gmail.com>
2024-04-26 12:01:37 +0530

    Commit

--- Complex_Device_Drivers/USR/INCLUDE/gpio.h
+++ Complex_Device_Drivers/USR/INCLUDE/gpio.h
@@ -5,6 +5,10 @@
 #define INPUT   0
 #define OUTPUT  1
 
+// Define logical states for digital output
+#define HIGH    1
+#define LOW     0
+
 // Function prototypes
 void gpio_init(int pin, int mode);
 void gpio_set(int pin, int value);
