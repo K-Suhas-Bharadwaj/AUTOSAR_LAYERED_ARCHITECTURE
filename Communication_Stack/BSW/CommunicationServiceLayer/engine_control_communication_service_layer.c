5ab7d6047b240d38f5bd7ac76cc1a7d4ad898c9f
Suhas Bharadwaj K <tak.kumara@gmail.com>
2024-04-26 15:59:10 +0530

    Update engine_control_communication_service_layer.c

--- Communication_Stack/BSW/CommunicationServiceLayer/engine_control_communication_service_layer.c
+++ Communication_Stack/BSW/CommunicationServiceLayer/engine_control_communication_service_layer.c
@@ -1,1 +1,17 @@
+#include "engine_control_communication_service_layer.h"
+#include "engine_control_communication_hardware_abstraction.h"
 
+void communicationServiceLayer_init() {
+    // Initialize communication hardware
+    initializeCommunicationHardware();
+}
+
+void sendFuelInjectionCommand(float amount) {
+    // Implementation of sending fuel injection command
+    sendDataOverCommunicationInterface(&amount, sizeof(amount));
+}
+
+void sendIgnitionTimingCommand(float timing) {
+    // Implementation of sending ignition timing command
+    sendDataOverCommunicationInterface(&timing, sizeof(timing));
+}
